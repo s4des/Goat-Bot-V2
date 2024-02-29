ee16da44bcc89454c911966b1b8f6009d4468f3a
tsama6610 <160520885+tsama6610@users.noreply.github.com>
2024-02-29 22:31:28 +0100

    Create ask.js

---
+++ ask.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+const Prefixes = [
+  '/ai',
+  'Salut',
+  'nemoo',
+  '+ai',
+  'Wendy',
+  'ai',
+  'ask',
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+      
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply("🦄Hé, je suis ton assistant virtuel 👻, pose-toi une question 🌺");
+        return;
+      }
+
+
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+ 
+    await message.reply(answer);
+
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
