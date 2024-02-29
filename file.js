92c4cbde73b095575a779683fd7acc645d3babc4
tsama6610 <160520885+tsama6610@users.noreply.github.com>
2024-02-29 20:15:45 +0100

    Create file.js

---
+++ file.js
@@ -0,0 +1,35 @@
+√cmd install file.js const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "file",
+    version: "1.0",
+    author: "OtinXShiva",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["100050637524770"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("-you have no permission toi do that", event.threadID, event.messageID);
+    }
+    
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("🌺Le nom du fichier 🦄 maître.", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
