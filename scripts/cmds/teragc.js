c2e436a6cc601d9e0aca576ff9ee402455374201
tsama6610 <160520885+tsama6610@users.noreply.github.com>
2024-02-29 20:09:46 +0100

    Create  teragc.js

---
+++ scripts/cmds/teragc.js
@@ -0,0 +1,54 @@
+÷cmd install teragc.js const fs = require('fs');
+const moment = require('moment-timezone');
+
+module.exports = {
+  config: {
+    name: "wendygc",
+    aliases: ["wendygc"],
+    version: "1.0",
+    author: "AceGun",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      vi: "",
+      en: "add user in thread"
+    },
+    longDescription: {
+      vi: "",
+      en: "add any user to bot owner group chat"
+    },
+    category: "chat box",
+    guide: {
+      en: "{pn}teragc"
+    }
+  },
+
+  onStart: async function ({ api, event, args }) {
+    const threadID = "7257899287658188";
+
+    try {
+      // Check if the user is already in the group chat
+      const threadInfo = await api.getThreadInfo(threadID);
+      const participants = threadInfo.participantIDs;
+
+      if (participants.includes(event.senderID)) {
+        api.sendMessage("⚠ | You're already in our group chat.", event.threadID);
+
+        // Set ⚠ reaction for already added user
+        api.setMessageReaction("⚠", event.messageID, "👍", api);
+      } else {
+        // If not, add the user to the group chat
+        await api.addUserToGroup(event.senderID, threadID);
+        api.sendMessage("✅ | You have been added to the nemo group chat.\check your Spam or Message Request folder if you can't find the group chat.", event.threadID);
+
+        // Set ✅ reaction for successfully added user
+        api.setMessageReaction("✅", event.messageID, "👍", api);
+      }
+    } catch (error) {
+      api.sendMessage("❌ | Failed to add you to the group chat.\join through the link:", event.threadID);
+
+      // Set ❌ reaction for failed adding user
+      api.setMessageReaction("❌", event.messageID, "👍", api);
+    }
+  }
+};
