7f49116f43c2dee64317f1ef755192f1d83f8263
tsama6610 <160520885+tsama6610@users.noreply.github.com>
2024-03-21 01:42:51 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -46,7 +46,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "🔞 System prefix: %1\n🚯 Your box chat prefix: %2"
 		}
 	},
 
