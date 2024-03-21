1fc7a0e743aa2805defbe9e57a9aac3d44f21293
tsama6610 <160520885+tsama6610@users.noreply.github.com>
2024-03-21 01:55:56 +0100

    Create 0ai.js

---
+++ scripts/cmds/0ai.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+
+const Prefixes = [
+  '√ai',
+  'Itachi',
+  'Nemo',
+  '+ai',
+  'nemo',
+  'ai',
+  'ask',
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply("📪Hé, je suis ton assistant virtuel 📱, pose-moi une question 🐰");
+        return;
+      }
+
+
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+
+    await message.reply({ body: `
+૮₍ ˃ ⤙ ˂ ₎ა
+./づᡕᠵ᠊ᡃ່࡚ࠢ࠘ ⸝່ࠡࠣ᠊߯᠆ࠣ࠘ᡁࠣ࠘᠊᠊ࠢ࠘~~~~~~~~~~
+======================    
+${answer}
+======================`,
+});
+
+   } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
